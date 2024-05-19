a1ae03eb912e447d8a9e9bd7f35d3428999cbff2
Chandrakant6 <141409100+Chandrakant6@users.noreply.github.com>
2024-05-19 10:35:56 +0530

    added storage-space to data.py

--- data.py
+++ data.py
@@ -8,5 +8,14 @@ data = {
 		'month' : 30 * 24 * pow(60, 2),
 		'year' : 365 * 24 * pow(60, 2),
 		'decade' : 10 * 365 * 24 * pow(60, 2),
+		},
+	'storageSpace : {
+		'bit' : 1/8,
+		'nibble' : 1/2,
+		'byte : 1,
+		'kiloByte' : pow(2,10) ,
+		'megaByte' : pow(2,20) ,
+		'giga-byte' : pow(2,30) ,
+		'tera-byte' : pow(2,40) ,
+		'peta-byte' : pow(2,50) ,
 		}
-	}
