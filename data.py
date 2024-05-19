124e54ee34d4c5e15e02c0ed344920e2e1777f12
Chandrakant6 <141409100+Chandrakant6@users.noreply.github.com>
2024-05-19 10:24:27 +0530

    added time to data.py

--- data.py
+++ data.py
@@ -1,1 +1,12 @@
-
+data = {
+	'time' : {
+		'second' : 1,
+		'minute' : 60,
+		'hour' : pow(60, 2),
+		'day' : 24 * pow(60, 2),
+		'week' : 7 * 24 * pow(60, 2),
+		'month' : 30 * 24 * pow(60, 2),
+		'year' : 365 * 24 * pow(60, 2),
+		'decade' : 10 * 365 * 24 * pow(60, 2),
+		}
+	}
