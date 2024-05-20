d0f5a96d90b4c55135dd0571d885150cb15486b3
Chandrakant6 <141409100+Chandrakant6@users.noreply.github.com>
2024-05-20 11:53:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from data import data
 
 def main(category :str, value :float, initialUnit: str, finalUnit :str):
-	if type(data[category]) is not dict:
+	if not isinstance(variable, dict):
 		return convert(category, value, initialUnit, finalUnit)
 	return dynamicConverter(category, value, initialUnit, finalUnit)
 
