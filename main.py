f7511774bca1f2ec1e07e0893d7119b423344f52
Chandrakant6 <141409100+Chandrakant6@users.noreply.github.com>
2024-05-20 11:38:18 +0530

    added main function in main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+from data import data
+
+def main(category :str, value :float, initialUnit: str, finalUnit :str):
+	if type(data[category]) is not dict:
+		return convert(category, value, initialUnit, finalUnit)
+	return dynamicConverter(category, value, initialUnit, finalUnit)
 
