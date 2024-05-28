7956600e55c0d19c3a139fb10bdf86d96c72d1b3
chandu <141409100+Chandrakant6@users.noreply.github.com>
2024-05-28 13:14:38 +0530

    create validate function and move validation to it in main.py

--- main.py
+++ main.py
@@ -3,12 +3,7 @@ import sys
 from data import data
 
 def main(category :str, value :float, initialUnit: str, finalUnit :str):
-	# category validation
-	if category not in data:
-		return f'Invalid category avilable categories are{data.keys}'
-	# value validation
-	elif not (isinstance(value, int) or isinstance(value, float)):
-		return f'invalid value datatype avilable datatypes are int or float' 
+	return validate(category, value, initialUnit, finalUnit)
 	elif not isinstance(category, dict):
 		return convert(category, value, initialUnit, finalUnit)
 	return dynamicConverter(category, value, initialUnit, finalUnit)
@@ -19,6 +14,21 @@ def converter(category :str, value :float, initialUnit: str, finalUnit :str):
 	
 def dynamicConverter(category :str, value :float, initialUnit: str, finalUnit :str): #temperature is dynamic 
 	return data[category][initialUnit][finalUnit](value)
+
+def validate(category, value, initialUnit, finalUnit):
+	# category validation
+	if category not in data:
+		return f'Invalid category avilable categories are{data.keys()}'
+		
+	# value validation
+	if not (isinstance(value, int) or isinstance(value, float)):
+		return f'invalid value datatype avilable datatypes are int or float' 
+
+	# unit validation
+	category = data[category]
+	if not ((initialUnit in category) or (finalUnit in category)):
+		return f'Invalid unit avilable units are{category.keys()}'
+	
 
 if __name__ == '__main__':
 	category, value, initialUnit, finalUnit, _ = sys.argv[2:6]
