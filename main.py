2c5fdb4c8e3af75869d102483992e7ca0e43679f
chandu <141409100+Chandrakant6@users.noreply.github.com>
2024-05-31 11:18:13 +0530

    change value validation

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ def main(category :str, value :float, initialUnit: str, finalUnit :str):
 	notValid = validate(category, value, initialUnit, finalUnit)
 	if notValid:
 		return notValid
+	try:
+		value = float(value)
+	except:
+		return f'invalid value value must be int or float'
 		
 	if not isinstance(data[category][initialUnit], dict):
 		return converter(category, value, initialUnit, finalUnit)
@@ -22,12 +26,11 @@ def validate(category, value, initialUnit, finalUnit):
 	if category not in data: # category validation
 		return f'Invalid category avilable categories are{data.keys()}'
 		
-	if not (isinstance(value, int) or isinstance(value, float)): # value validation
-		return f'invalid value datatype avilable datatypes are int or float'
+	# if not (isinstance(value, (int,float)): # value validation
+	# 	return f'invalid value datatype avilable datatypes are int or float'
 
 	if not ((initialUnit in data[category]) or (finalUnit in data[category])): # unit validation
 		return f'Invalid unit avilable units are{category.keys()}'
 
-if __name__ == '__main__':
-	category, value, initialUnit, finalUnit = sys.argv[2:6]
-	return main(category, float(value), initialUnit, finalUnit)
+if __name__ == '__main__': 
+	return main(*sys.argv[2:6])
